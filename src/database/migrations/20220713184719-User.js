5d21dd49d96fe4f2ec3000fc4dff4812f7137eaf
Pedrin <pedro515vieira@gmail.com>
2022-07-14 16:33:25 -0300

    :heavt_check_mark: feat: Requisito 1, Criando as migrations de todas as tabelas

---
+++ src/database/migrations/20220713184719-User.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      displayName: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
